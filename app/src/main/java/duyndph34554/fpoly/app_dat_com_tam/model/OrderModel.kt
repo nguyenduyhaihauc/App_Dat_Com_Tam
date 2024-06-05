21ea87fc349a9988c40dd4a7177f9f90ed47d582
nguyenduyhaihauc <nguyenduyhaihauc@gmail.com>
2024-06-05 19:44:35 +0700

    update fun manageScreen

--- app/src/main/java/duyndph34554/fpoly/app_dat_com_tam/model/OrderModel.kt
+++ app/src/main/java/duyndph34554/fpoly/app_dat_com_tam/model/OrderModel.kt
@@ -1,4 +1,7 @@
 package duyndph34554.fpoly.app_dat_com_tam.model
 
-class OrderModel(var nameOrder: String, var totalAmount: Double, var status: Boolean) {
+class OrderModel(
+    var nameOrder: String,
+    var totalAmount: Double,
+    var status: Boolean) {
 }
