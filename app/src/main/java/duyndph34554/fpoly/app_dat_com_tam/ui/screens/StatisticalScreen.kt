dfb330001fefc393051fe4fb82221060718f832e
BuiCuong <truongquyet137@gmail.com>
2024-06-07 13:11:14 +0700

    type rice ui

--- app/src/main/java/duyndph34554/fpoly/app_dat_com_tam/ui/screens/StatisticalScreen.kt
+++ app/src/main/java/duyndph34554/fpoly/app_dat_com_tam/ui/screens/StatisticalScreen.kt
@@ -1,10 +1,46 @@
 package duyndph34554.fpoly.app_dat_com_tam.ui.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import duyndph34554.fpoly.app_dat_com_tam.R
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun StatisticalScreen (navController: NavController) {
+    Scaffold (
+        topBar = {
+            TopAppBar(navController = navController,
+                iconLeft = R.drawable.logo_home,
+                title = "Com tứm đim"
+            )
+        },
+        content = {
+            ContentSupport()
+        }
+    )
+}
 
 @Composable
-fun StatisticalScreen() {
-    Text(text = "Thong ke", color = Color.Yellow)
+fun Statistical() {
+
 }
