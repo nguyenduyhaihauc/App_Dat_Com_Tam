6f26633f097e87df4738879633c95a115e29adde
BuiCuong <truongquyet137@gmail.com>
2024-06-11 00:28:26 +0700

    insert data account

--- app/src/main/java/duyndph34554/fpoly/app_dat_com_tam/room/database/AccountDb.kt
+++ app/src/main/java/duyndph34554/fpoly/app_dat_com_tam/room/database/AccountDb.kt
@@ -4,13 +4,12 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-
+import androidx.sqlite.db.SupportSQLiteDatabase
 import duyndph34554.fpoly.app_dat_com_tam.room.dao.AccountDao
-import duyndph34554.fpoly.app_dat_com_tam.room.database.AccountDb.Companion.getInstance
 import duyndph34554.fpoly.app_dat_com_tam.room.model.Account
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.launch
 
 @Database(entities = [Account::class], version = 1, exportSchema = false)
 abstract class AccountDb : RoomDatabase() {
@@ -26,51 +25,56 @@ abstract class AccountDb : RoomDatabase() {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AccountDb::class.java,
-                    "account"
+                    "account2"
                 )
                     .fallbackToDestructiveMigration()
+                    .addCallback(AccountDatabaseCallback())
                     .build()
                 INSTANCE = instance
                 instance
             }
         }
 
-        fun populateInitialData(context: Context) {
-            runBlocking {
-                withContext(Dispatchers.IO) {
-                    val db = getInstance(context)
-                    val accountDao = db.accountDao()
-
-                        val account1 = Account(
-                            userName = "quyet",
-                            passWord = "quyet",
-                            role = "admin",
-                            address = "Thanh Hóa",
-                            fullName = "TVQ",
-                            imageUrl = null
-                        )
-                        val account2 = Account(
-                            userName = "duy",
-                            passWord = "duy",
-                            role = "admin",
-                            address = "Nam Định",
-                            fullName = "NDD",
-                            imageUrl = null
-                        )
-                        val account3 = Account(
-                            userName = "toán",
-                            passWord = "toan",
-                            role = "admin",
-                            address = "Phú Thọ",
-                            fullName = "NTT",
-                            imageUrl = null
-                        )
-                        accountDao.insertAccount(account1)
-                        accountDao.insertAccount(account2)
-                        accountDao.insertAccount(account3)
-
+        private class AccountDatabaseCallback : RoomDatabase.Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                INSTANCE?.let { database ->
+                    CoroutineScope(Dispatchers.IO).launch {
+                        populateDatabase(database.accountDao())
+                    }
                 }
             }
+        }
+
+        suspend fun populateDatabase(accountDao: AccountDao) {
+            val account1 = Account(
+                userName = "quyet",
+                passWord = "quyet",
+                role = "admin",
+                address = "Thanh Hóa",
+                fullName = "TVQ",
+                imageUrl = null
+            )
+            val account2 = Account(
+                userName = "duy",
+                passWord = "duy",
+                role = "admin",
+                address = "Nam Định",
+                fullName = "NDD",
+                imageUrl = null
+            )
+            val account3 = Account(
+                userName = "toan",
+                passWord = "toan",
+                role = "admin",
+                address = "Phú Thọ",
+                fullName = "NTT",
+                imageUrl = null
+            )
+
+            accountDao.insertAccount(account1)
+            accountDao.insertAccount(account2)
+            accountDao.insertAccount(account3)
         }
     }
 }
